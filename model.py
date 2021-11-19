8bf9fdd207b9b96bca11a6ffb01dbb061e0519c1
KavinduLiyanage <sandeepa8956@gmail.com>
2021-11-19 23:33:59 +0530

    Changed app

--- model.py
+++ model.py
@@ -4,36 +4,34 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
 
-dataset = pd.read_csv('hiring.csv')
+dataset = pd.read_csv('merge-60de01b2adf55.csv')
 
-dataset['experience'].fillna(0, inplace=True)
-
-dataset['test_score'].fillna(dataset['test_score'].mean(), inplace=True)
-
-X = dataset.iloc[:, :3]
+dataset.drop(['line','order','time','date'], axis=1, inplace=True)
 
 #Converting words to integer values
-def convert_to_int(word):
-    word_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,
-                'nine':9, 'ten':10, 'eleven':11, 'twelve':12, 'zero':0, 0: 0}
-    return word_dict[word]
-
-X['experience'] = X['experience'].apply(lambda x : convert_to_int(x))
-
-y = dataset.iloc[:, -1]
+X = dataset.iloc[:, 0:2].values
+y = dataset.iloc[:, 2].values
 
 #Splitting Training and Test Set
-#Since we have a very small dataset, we will train our model with all availabe data.
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
 #Fitting model with trainig data
-regressor.fit(X, y)
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators=20, random_state=0)
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
 
 # Saving model to disk
 pickle.dump(regressor, open('model.pkl','wb'))
 
 # Loading model to compare the results
 model = pickle.load(open('model.pkl','rb'))
-print(model.predict([[2, 9, 6]]))
+print(model.predict([[-22.88163, -43.29324]]))
