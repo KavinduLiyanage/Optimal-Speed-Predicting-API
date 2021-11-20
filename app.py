088f2767901839bc56b0207babed0ef3b4eabf38
KavinduLiyanage <sandeepa8956@gmail.com>
2021-11-20 12:28:57 +0530

    Added changes

--- app.py
+++ app.py
@@ -1,14 +1,49 @@
-from flask import Flask, render_template, request, flash
+# from flask import Flask, render_template, request, flash
 
-app = Flask(__name__)
+# app = Flask(__name__)
 # app.secret_key = "manbearpig_MUDMAN888"
 
-@app.route("/hello")
-def index():
-	flash("what's your name?")
-	return render_template("index.html")
+# @app.route("/")
+# def index():
+# 	flash("what's your name?")
+# 	return render_template("index.html")
 
-@app.route("/greet", methods=['POST', 'GET'])
-def greeter():
-	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
-	return render_template("index.html")
+# @app.route("/greet", methods=['POST', 'GET'])
+# def greeter():
+# 	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+# 	return render_template("index.html")
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+# import pickle
+from flask_cors import CORS
+
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def home():
+    return "This is Optimal speed predicting app"
+# CORS(app)
+# model = pickle.load(open('model.pkl', 'rb'))
+
+# @app.route('/', methods=['GET'])
+# def home():
+#     return jsonify({"response":"This is Optimal speed predicting app"})
+
+# @app.route('/predict',methods=['POST'])
+# def predict():  
+#     int_features = [float(x) for x in request.form.values()]
+    
+#     print('Vehicle location = ',int_features)
+    
+#     final_features = [np.array(int_features)]
+    
+#     prediction = model.predict(final_features)
+    
+#     output = round(prediction[0])
+    
+#     print('Predict speed = ', output)
+
+#     return format(output)
+
+if __name__ == "__main__":
+    # app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run()
